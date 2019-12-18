56cec87184f241b95c46f8946772b9504aa07700
Ari <gryphon150@gmail.com>
2019-12-18 15:06:01 -0800

    Updates

---
+++ precache-manifest.a47fb0a79bf1052a19718a4a0bf32ea7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102a9bdc7b7905bae3376b64d35d7927",
+    "url": "/arigreif/index.html"
+  },
+  {
+    "revision": "3c299a360585d8bc1f36",
+    "url": "/arigreif/static/css/main.b6026cfb.chunk.css"
+  },
+  {
+    "revision": "7b94d91a425907141d1f",
+    "url": "/arigreif/static/js/2.3757b08e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/arigreif/static/js/2.3757b08e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c299a360585d8bc1f36",
+    "url": "/arigreif/static/js/main.adebb720.chunk.js"
+  },
+  {
+    "revision": "abab15867e0b0647f5a2",
+    "url": "/arigreif/static/js/runtime-main.b6dd3970.js"
+  },
+  {
+    "revision": "a0d727e0692211f13f153972da29aba8",
+    "url": "/arigreif/static/media/AdCategories.a0d727e0.png"
+  },
+  {
+    "revision": "9b581e7ce36aaa228be6ed6ccc67fe18",
+    "url": "/arigreif/static/media/AdCreate.9b581e7c.png"
+  },
+  {
+    "revision": "576222c9a3465cabf645f5ac758164c5",
+    "url": "/arigreif/static/media/AdList.576222c9.png"
+  },
+  {
+    "revision": "047ee8026a3597e47abe09aabca63aab",
+    "url": "/arigreif/static/media/AdventureHomepage.047ee802.png"
+  },
+  {
+    "revision": "8299ca3d19671cfba8a226072ff0b45d",
+    "url": "/arigreif/static/media/header.8299ca3d.jpeg"
+  },
+  {
+    "revision": "47110af4c24ee591936734ceb8b690f9",
+    "url": "/arigreif/static/media/jtAddNote.47110af4.png"
+  },
+  {
+    "revision": "f0aa3d0852919c1df8b8418186da485f",
+    "url": "/arigreif/static/media/jtDetail.f0aa3d08.png"
+  },
+  {
+    "revision": "6b83b815676939fdfc97a240595271d6",
+    "url": "/arigreif/static/media/jtHomepage.6b83b815.png"
+  },
+  {
+    "revision": "60dba796a514fd67e57c2ecd101d7239",
+    "url": "/arigreif/static/media/jtProfile.60dba796.png"
+  },
+  {
+    "revision": "ff030b9d10a8c83d1677aaa81579c9ae",
+    "url": "/arigreif/static/media/wpHome.ff030b9d.png"
+  },
+  {
+    "revision": "8114e93b029c2b276e343c2c5904df57",
+    "url": "/arigreif/static/media/wpProfile.8114e93b.png"
+  },
+  {
+    "revision": "8cd134ec5d584afdfcb2d872ae914349",
+    "url": "/arigreif/static/media/wpPrompt.8cd134ec.png"
+  }
+]);
